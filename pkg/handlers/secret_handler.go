e473d945566f846f3be776ba086dad0ec6204d4a
Minh Vu <minhphanh.vu@gmail.com>
2022-02-13 15:37:39 -0600

    Hash the string and send it back to the client

--- pkg/handlers/secret_handler.go
+++ pkg/handlers/secret_handler.go
@@ -3,15 +3,52 @@ package handlers
 import (
 	"net/http"
 	"io"
+	"encoding/json"
+	"fmt"
+	"crypto/md5"
 )
 
+type Secret struct {
+	PlainText string `json:"plain_text"`
+}
+
+type SecretResponse struct {
+	Id string `json:"id"`
+}
+
 func secretHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		io.WriteString(w, "Method Post")
-	} else if r.Method == "GET" {
+	method := r.Method
+	if method == "POST" {
+		createSecret(w, r)
+	} else if method == "GET" {
 		io.WriteString(w, "Method GET")
 	} else {
 		w.WriteHeader(405);
 		io.WriteString(w, "Method is not allowed.")
 	}
+}
+
+func getHash(plainText string) string {
+	text := []byte(plainText)
+	return fmt.Sprintf("%x", md5.Sum(text))
+}
+
+func createSecret(w http.ResponseWriter, r *http.Request) {
+	s := Secret{}
+	err := json.NewDecoder(r.Body).Decode(&s)
+
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	hashDigest := getHash(s.PlainText)
+	secretResponse := SecretResponse{Id: hashDigest}
+
+	res, err := json.Marshal(&secretResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
 }
